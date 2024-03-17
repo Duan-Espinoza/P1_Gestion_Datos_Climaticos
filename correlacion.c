78fcb67d67639079f7feeacd84734aab8051e477
Duan Antonio Espinoza Olivares <75913012+Duan-Espinoza@users.noreply.github.com>
2024-03-16 19:02:42 -0600

    correlacion Pearson

---
+++ correlacion.c
@@ -0,0 +1,24 @@
+#include "correlacion.h"
+#include <math.h>
+
+double calcularCorrelacionPearson(double *x, double *y, int n) {
+    double sum_x = 0.0, sum_y = 0.0, sum_xy = 0.0;
+    double sum_x_squared = 0.0, sum_y_squared = 0.0;
+
+    for (int i = 0; i < n; i++) {
+        sum_x += x[i];
+        sum_y += y[i];
+        sum_xy += x[i] * y[i];
+        sum_x_squared += x[i] * x[i];
+        sum_y_squared += y[i] * y[i];
+    }
+
+    double correlacion_numerador = n * sum_xy - sum_x * sum_y;
+    double correlacion_denominador = sqrt((n * sum_x_squared - sum_x * sum_x) * (n * sum_y_squared - sum_y * sum_y));
+
+    if (correlacion_denominador == 0) {
+        return 0; // Evitar división por cero
+    }
+
+    return correlacion_numerador / correlacion_denominador;
+}
---
+++ headers/correlacion.h
@@ -0,0 +1,9 @@
+#ifndef CORRELACION_H
+#define CORRELACION_H
+
+// Incluye las bibliotecas necesarias, como <math.h> si necesitas la función sqrt()
+
+// Declaración de la función para calcular el coeficiente de correlación de Pearson
+double calcularCorrelacionPearson(double *x, double *y, int n);
+
+#endif /* CORRELACION_H */
